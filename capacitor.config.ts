655cd80980e01bf833c26f6e899a099d91898d22
Joseph Heininge <joeheininge@gmail.com>
2025-08-19 17:46:13 -0700

    Added Capacitor libraries

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ai.nursehelp.app',
+  appName: 'NCLEX Tutor',
+  webDir: 'public'
+};
+
+export default config;
