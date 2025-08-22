b5f73ef021008bf55f60c686bc090fecbadf0ce0
Joseph Heininge <joeheininge@gmail.com>
2025-08-22 12:24:03 -0700

    Configured static build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'ai.nursehelp.app',
   appName: 'NCLEX Tutor',
-  webDir: 'public'
+  webDir: 'out'
 };
 
 export default config;
